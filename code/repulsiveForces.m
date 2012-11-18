fdc2122c09ac0488f22d8e86a44648e395ab4572
Benedek Vartok <vartokb@student.ethz.ch>
2012-11-18 15:03:22 +0100

    - Created toy simulation loop with plotting for testing - Implemented first version of agent-agent interaction force

--- code/repulsiveForces.m
+++ code/repulsiveForces.m
@@ -1,4 +1,38 @@
 %calculates the repulsive force of each agent
-function repulsiveForces()
+function forces = repulsiveForces(agentsPos, agentsVel)
 
+% Constants (TODO: should be assigned per agent):
+A = 0.1;      % repulsion force factor
+B = 0.1;      % distance falloff factor
+r = 0.3;      % agent radius
+k = 1e2;      % contact repulsion coefficient
+kappa = 1e2;  % sliding friction coefficient
 
+n = length(agentsPos);
+forces = zeros(n, 2);
+
+for agentIdx = 1:n
+    for otherIdx = 1:n
+        % Skip if the two agents are the same:
+        if otherIdx == agentIdx
+            continue
+        end
+        
+        % Calculate different vectors between agent-agent pair:
+        dPos = agentsPos(agentIdx,:) - agentsPos(otherIdx,:);
+        d = norm(dPos);
+        
+        % Distance cutoff:
+        if(d > 2), continue; end
+        
+        normal = dPos / d;
+        tangent = [-normal(2), normal(1)];
+        
+        dVel = dot(agentsVel(otherIdx,:) - agentsVel(agentIdx,:), tangent);
+        
+        % Calculate force for one agent-agent pair:
+        forces(agentIdx,:) = forces(agentIdx,:) + ...
+            (A * exp((r-d)/B) + k * max(0, r-d)) * normal + ...
+            kappa * max(0, r-d) * dVel * tangent;
+    end
+end
--- code/simulate.m
+++ code/simulate.m
@@ -9,17 +9,39 @@ function simulate(configFile)
 
 %Timestep (in seconds)
 dt = 0.1;
-duration = 60;
+duration = 120;
 time = 0;
 
 %Initialize the environment
 
+% Agents:
+% Each row consists of an x and y component.
+nAgents = 50;
+agentsPos = [10 * (rand(nAgents, 2) - 0.5)];
+agentsVel = [rand(nAgents, 2) - 0.5];
+
+targetVel = 0.2;
+
 %Simulation loop
 while(time<duration)
+    % Calculate forces:
+    forces = repulsiveForces(agentsPos, agentsVel);
+    % Go to center:
+    forces = forces - 0.01 * agentsPos;
+    % Target velocity:
+    tmp = targetVel - sqrt(sum(agentsVel.^2,2));
+    forces = forces + 5 * tmp(:,ones(1,2)) .* agentsVel;
+    
+    % Progress agents with Leap-Frog integration:
+    agentsVel = agentsVel + dt * forces;
+    agentsPos = agentsPos + dt * agentsVel;
 
+    % Plot agent positions:
+    plot(agentsPos(:,1), agentsPos(:,2), 'o');
+    axis([-10, 10, -10, 10]);
+    drawnow;
 
 	time = time+dt;
 end
+
 %Output processing
-
-
