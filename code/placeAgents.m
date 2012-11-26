1cd06454f89609b986db99b2295d9b0e7509849f
jogi91 <jweinbuch@gmx.ch>
2012-11-26 18:42:46 +0100

    Worked on placement, imported a placing method from Multilevel evacuation

--- code/placeAgents.m
+++ code/placeAgents.m
@@ -3,18 +3,40 @@ function data = placeAgents(data, agentCount, floorNumber, spawnZoneNumber)
 
 %get a random Radius
 function r = getRadius();
-	r = data.r_min + (data.r_max-data.r_min)*rand(1);	
+r = data.r_min + (data.r_max-data.r_min)*rand(1);	
 end
 
-s = size(data.agents);
-numAgents = s(2);
-[xSpots,ySpots] = find(data.floor(floorNumber).spawnZone{spawnZoneNumber} ==1)
+
+numAgents = length(data.agents);
+[xSpots,ySpots] = find(data.floor(floorNumber).spawnZone{spawnZoneNumber} ==1); %where Can I place Agents
+%Convert to metric
+xSpots = xSpots*data.meter_per_pixel;
+ySpots = ySpots*data.meter_per_pixel;
+
 for i=(numAgents+1):numAgents+agentCount+1
-	%TODO: Randomly Populate the Area
-	data.agents(i).r = getRadius; %How fat is the Agent?
-	data.agents(i).v0 = data.v0; %How fast wants the Agent go?
-	data.agents(i).f = [0 0]; % Use the Force, Luke!
-	data.agents(i).v = [0 0]; %Run Forest, run!
-	
+	data.floor(floorNumber).agents(i).r = getRadius; %How fat is the Agent?
+	data.floor(floorNumber).agents(i).v0 = data.v0; %How fast wants the Agent go?
+	data.floor(floorNumber).agents(i).f = [0 0]; % Use the Force, Luke!
+	data.floor(floorNumber).agents(i).v = [0 0]; %Run Forest, run!
+
 	%Where am I?
+
+	while tries > 0
+	% randomly pick a spot and check if it's free
+		idx = randi(length(xSpots));
+		data.floor(floorNumber).agents(i).pos = [x(idx)*];
+		if checkForIntersection(data, i, cur_agent) == 0
+			tries = -1; % leave the loop
+		end
+		tries = tries - 1;
+		end
+		if tries > -1
+			%remove the last agent
+			data.floor(i).agents = data.floor(i).agents(1:end-1);
+		end
+	end
+
+	%Could the Agent be placed
+end
+
 end
