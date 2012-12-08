2e0b02432e983de9c96e9d7fbd5a780a2c02bfab
Benedek Vartok <vartokb@student.ethz.ch>
2012-12-08 10:37:49 +0100

    - added file I forgot in last commit

---
+++ code/plotFloor.m
@@ -0,0 +1,29 @@
+function plotFloor(data)
+% Draws the floor layout with the agents.
+
+hold off;
+imagesc(1 * data.floor.wall);
+colormap([1 1 1; 0 0 0]);
+shading flat;
+hold on;
+
+% Prepare circle data for drawing:
+t = linspace(0, 2*pi, 16);
+x = cos(t);
+y = sin(t);
+
+for ai = 1:length(data.agents)
+%     plot(data.agents(ai).p(:,1) / config.meter_per_pixel, ...
+%          data.agents(ai).p(:,2) / config.meter_per_pixel, 'o');
+    % Draw an agent as a circle with the right middle point and radius:
+    x0 = data.agents(ai).p(1);
+    y0 = data.agents(ai).p(2);
+    r  = data.agents(ai).r;
+    line((r * x + x0) / data.meter_per_pixel, ...
+         (r * y + y0) / data.meter_per_pixel);
+end
+
+drawnow;
+
+end
+
