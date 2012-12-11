65be73a20eae696f0fda381bf078301786c506bb
Benedek Vartok <vartokb@student.ethz.ch>
2012-12-11 07:12:24 +0100

    - changed final exit occupation plots to be shown in separate plots

--- code/plotExitedAgents.m
+++ code/plotExitedAgents.m
@@ -1,18 +1,18 @@
 function data = plotExitedAgents(config, data)
 % Plots the time series of the exit occupations.
 
-figure;
 
 for i = 1 : data.num_exits
-    subplot(data.num_exits, 1, i);
+%     subplot(data.num_exits, 1, i);
+    figure;
     axis([0 data.finish_time 0 config.exit_capacities(i)]);
     plot(data.agents_exited_time_series(:,1), ...
          data.agents_exited_time_series(:,1+i));
     xlabel('time [s]');
     ylabel('# agents');
     title(sprintf('Agents on boat %i', i));
+    print('-depsc2', sprintf('frames/%s_exit_occupation_%d.eps', ...
+                             data.frame_basename, i));
 end
-
-print('-depsc2', sprintf('frames/%s_exit_occupation.eps', data.frame_basename));
 
 end
