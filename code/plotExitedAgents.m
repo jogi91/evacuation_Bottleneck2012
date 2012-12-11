9edf8ace66cc7b7123e6399463eece01967842c7
Benedek Vartok <vartokb@student.ethz.ch>
2012-12-11 07:19:16 +0100

    - added plot showing total number of agents on boats

--- code/plotExitedAgents.m
+++ code/plotExitedAgents.m
@@ -1,7 +1,7 @@
 function data = plotExitedAgents(config, data)
 % Plots the time series of the exit occupations.
 
-
+% Plot separate exits:
 for i = 1 : data.num_exits
 %     subplot(data.num_exits, 1, i);
     figure;
@@ -14,5 +14,17 @@ for i = 1 : data.num_exits
     print('-depsc2', sprintf('frames/%s_exit_occupation_%d.eps', ...
                              data.frame_basename, i));
 end
+
+% Plot totals:
+figure;
+axis([0 data.finish_time 0 config.exit_capacities(i)]);
+plot(data.agents_exited_time_series(:,1), ...
+     sum(data.agents_exited_time_series(:,2:data.num_exits+1), 2));
+xlabel('time [s]');
+ylabel('# agents');
+title('Total number of agents on boats');
+print('-depsc2', sprintf('frames/%s_exit_occupation_total.eps', ...
+                         data.frame_basename));
+
 
 end
